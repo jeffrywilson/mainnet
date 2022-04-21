181571e3b4d63f33f9f4b74d11789374362a4697
tomgroup <conda0905@gmail.com>
2022-04-21 21:29:45 +0900

    add stake and bond

--- src/components/bond/Bond.tsx
+++ src/components/bond/Bond.tsx
@@ -3,11 +3,35 @@ import React, { FC, Fragment } from "react";
 const Bond: FC<{}> = () => {
   return (
     <Fragment>
-      <h3>Tab Three</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
-        sint illum iusto nostrum cumque qui voluptas tenetur inventore ut quis?
-      </p>
+      <div className="mb-5">
+        <h3>Deposit</h3>
+        <div className="flex flex-row">
+          <select name="deposit" className="basis-1/4">
+            <option value="USDT">USDT</option>
+            <option value="USDC">USDC</option>
+            <option value="DAI">DAI</option>
+          </select>
+          <input type="text" className="bg-[#ce8d3e] h-10 w-full !pl-28"/>
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <h3>Receive</h3>
+        <div className="flex flex-row">
+          <input type="text" className="bg-[#d8a064] h-10 w-full" placeholder="STABL3:"/>
+        </div>
+      </div>
+
+      <div>
+        <h3>Earned</h3>
+        <div className="flex flex-row mb-3">
+          <input type="text" className="basis-3/4 bg-[#d8a064] mr-10" placeholder="USDT/USDC/DAI:" />
+          <button className="basis-1/4">
+            {`WITHDRAW`}
+          </button>
+        </div>
+      </div>
+
     </Fragment>
   );
 };
--- src/components/lending/Lending.tsx
+++ src/components/lending/Lending.tsx
@@ -3,35 +3,45 @@ import React, { FC, Fragment } from "react";
 const Lending: FC<{}> = () => {
   return (
     <Fragment>
-      <div>
+      <div className="mb-5">
         <h3>Deposit</h3>
         <div className="flex flex-row">
-          <input type="text" className="basis-1 bg-[#ce8d3e]"/>
+          <select name="deposit" className="basis-1/4">
+            <option value="USDT">USDT</option>
+            <option value="USDC">USDC</option>
+            <option value="DAI">DAI</option>
+          </select>
+          <input type="text" className="bg-[#ce8d3e] h-10 w-full !pl-28"/>
         </div>
       </div>
 
       <div>
         <h3>Earned</h3>
         <div className="flex flex-row mb-3">
-          <input type="text" className="basis-2/3 bg-[#d8a064]" placeholder="USDT/USDC/DAI:" />
-          <button className="basis-1/3 ml-5">
+          <input type="text" className="basis-3/4 bg-[#d8a064] mr-10" placeholder="USDT/USDC/DAI:" />
+          <button className="basis-1/4">
             {`WITHDRAW`}
           </button>
         </div>
         <div className="flex flex-row mb-3">
-          <input type="text" className="basis-2/3 bg-[#d8a064]" placeholder="UCD:" />
-          <button className="basis-1/3 ml-5">
+          <input type="text" className="basis-3/4 bg-[#d8a064] mr-10" placeholder="UCD:" />
+          <button className="basis-1/4">
             {`WITHDRAW`}
           </button>
         </div>
         <div className="flex flex-row">
-          <input type="text" className="basis-2/3 bg-[#d8a064]" placeholder="STABLE3:" />
-          <button className="basis-1/3 ml-5">
+          <input type="text" className="basis-3/4 bg-[#d8a064] mr-10" placeholder="STABLE3:" />
+          <button className="basis-1/4">
             {`WITHDRAW`}
           </button>
         </div>
         
-      </div>      
+        <div className="flex justify-center mt-10">
+          <button className="w-60">
+            {`WITHDRAW ALL`}
+          </button>
+        </div>
+      </div>
     </Fragment>
   );
 };
